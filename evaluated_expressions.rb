bdbf442efe51e40c859c8a46b299026ce6629407
DustinMiner <dustinjminer@outlook.com>
2019-02-09 14:55:05 -0600

    Create evaluated_expressions

---
+++ evaluated_expressions.rb
@@ -0,0 +1,9 @@
+puts 3 + 3 + 3 * 10 ==36
+puts (2 * 4) / 2 - 4 == 0
+puts "hello" * 3 == "hello" * 3
+puts "hello" + "3" == "hello" + "3"
+puts !true == !true
+"false" == "false"
+puts true && true
+puts false || true
+puts false || true && 3 == 3
