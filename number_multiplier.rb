7722d7c8493bfe3084746c3006e1fe58b4e7ce33
DustinMiner <dustinjminer@outlook.com>
2019-02-10 07:28:21 -0600

    Create fullname.rb

--- number_multiplier.rb
+++ number_multiplier.rb
@@ -1,3 +1,5 @@
-puts "Enter two numbers!"
-numbers = x
-numbers = y
+puts "Enter a Number!"
+first_number = gets.chomp.to_i
+puts "Enter a second number!"
+second_number = gets.chomp.to_i
+puts first_number * second_number
