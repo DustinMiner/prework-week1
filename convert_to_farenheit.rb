9976fda56f93ed0e23e01d6278beb75acf45ca52
DustinMiner <dustinjminer@outlook.com>
2019-02-11 08:07:55 -0600

    Create delete_vowel.rb

---
+++ convert_to_farenheit.rb
@@ -0,0 +1,9 @@
+puts "Enter the Degree in c:"
+c = gets.chomp
+
+def celsius_fahrenheit (f) 
+  return f = ( c * 9 / 5) + 32
+end
+
+answer = "The #{celsius_fahrenheit (f)} equivalent is"
+puts answer
---
+++ delete_vowels.rb
@@ -0,0 +1,9 @@
+def delete_vowels
+  puts "Hello, type a word please."
+  name = gets.chomp
+  string = 
+  puts string.delete('aeiou')  
+
+end
+
+delete_vowels
