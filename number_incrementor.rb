fa1498998ed31dec039e03f0d4b312ba5840de86
DustinMiner <dustinjminer@outlook.com>
2019-02-10 21:11:10 -0600

    Refactor number_incrementor program to be a method

--- number_incrementor.rb
+++ number_incrementor.rb
@@ -1,12 +1,15 @@
-puts "Enter a Number!"
-number = gets.chomp.to_i
-puts number
-puts number + 1
-puts number + 2
-puts number + 3
-puts number + 4
-puts number + 5
-puts number + 6
-puts number + 7
-puts number + 8
-puts number + 9
+def increment(number)
+  puts number
+  puts number + 1
+  puts number + 2
+  puts number + 3
+  puts number + 4
+  puts number + 5
+  puts number + 6
+  puts number + 7
+  puts number + 8
+  puts number + 9
+
+end
+
+increment (40)
