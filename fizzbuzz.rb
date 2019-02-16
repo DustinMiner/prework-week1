8f3e31921fb45a7ff498ff5b3dc07928ba3eae9a
DustinMiner <dustinjminer@outlook.com>
2019-02-15 20:27:22 -0600

    Create my_reverse.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,18 @@
-def fizzbuzz (number, number_of_iterations)
-  number_of_iterations.times do
-    puts number
-    number += 1
-    if number / 3 
+def fizzbuzz(number)
+    divisibleBy3 = (number % 3 == 0)
+    divisibleBy5 = (number % 5 == 0)
 
-     
-     end
-    
-  end
+    case
+        when divisibleBy3 && divisibleBy5
+            puts "FizzBuzz"
+        when divisibleBy3
+            puts "Fizz"
+        when divisibleBy5
+            puts "Buzz"
+        else 
+            puts number
+    end
+end
 
-  fizzbuzz(1, 100)
+
+(1..100).each {|n| fizzbuzz n}
